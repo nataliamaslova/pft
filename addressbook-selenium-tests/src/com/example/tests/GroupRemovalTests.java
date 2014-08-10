29cc1bf750ba44ae906ad9eb21aee37a5a2e7f0d
maslova <maslova.nd@gmail.com>
2014-08-10 20:15:31 +0300

    real verification to GroupRemovalTests is added

--- addressbook-selenium-tests/src/com/example/tests/GroupRemovalTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupRemovalTests.java
@@ -2,6 +2,11 @@ package com.example.tests;
 
 import org.testng.annotations.Test;
 
+import java.util.Collections;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
 /**
  * Created by nataliamaslova on 8/2/2014.
  */
@@ -11,7 +16,20 @@ public class GroupRemovalTests extends TestBase {
     public void deleteSomeGroup() {
         app.getNavigationHelper().openMainPage();
         app.getNavigationHelper().gotoGroupsPage();
-        app.getGroupHelper().deleteGroup(1);
+
+        // save old state
+        List<GroupData> oldList = app.getGroupHelper().getGroups();
+
+        // actions
+        app.getGroupHelper().deleteGroup(0);
         app.getNavigationHelper().gotoGroupsPage();
+
+        //save new state
+        List<GroupData> newList = app.getGroupHelper().getGroups();
+
+        // compare states
+        oldList.remove(0);
+        Collections.sort(oldList);
+        assertEquals(newList, oldList);
     }
 }
