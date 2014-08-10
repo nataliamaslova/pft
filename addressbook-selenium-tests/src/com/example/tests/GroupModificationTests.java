a1df3cf9af3b062aca53dcfdba6afbc82c64668c
maslova <maslova.nd@gmail.com>
2014-08-10 20:31:34 +0300

    verification in GroupModificationTests is added

--- addressbook-selenium-tests/src/com/example/tests/GroupModificationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupModificationTests.java
@@ -2,6 +2,11 @@ package com.example.tests;
 
 import org.testng.annotations.Test;
 
+import java.util.Collections;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
 /**
  * Created by nataliamaslova on 8/2/2014.
  */
@@ -11,12 +16,26 @@ public class GroupModificationTests extends TestBase {
     public void modifySomeGroup() {
         app.getNavigationHelper().openMainPage();
         app.getNavigationHelper().gotoGroupsPage();
-        app.getGroupHelper().initGroupModification(1);
+
+        // save old state
+        List<GroupData> oldList = app.getGroupHelper().getGroups();
+
+        // actions
+        app.getGroupHelper().initGroupModification(0);
         GroupData group = new GroupData();
         group.name = "new name";
         app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getNavigationHelper().gotoGroupsPage();
+
+        //save new state
+        List<GroupData> newList = app.getGroupHelper().getGroups();
+
+        // compare states
+        oldList.remove(0);
+        oldList.add(group);
+        Collections.sort(oldList);
+        assertEquals(newList, oldList);
     }
 
 }
